86e999afcb6a15e7739acecff22b16df9c4b712f
Ruben Hernandez Alvarado <rubhernandez42@gmail.com>
2023-01-29 00:52:39 -0600

    cripto table added

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,8 @@
-import { useEffect, useState } from 'react'
+import Cripto from './Components/Cripto'
 
 function App() {
-  const API_URL = import.meta.env.VITE_API_URL
-
-  const [criptos, setCriptos] = useState()
-
-  useEffect(() => {
-    fetch(`${API_URL}assets`)
-      .then((response) => response.json())
-      .then((data) => {
-        setCriptos(data.data)
-      })
-      .catch(() => {
-        console.error('La peticion fallo')
-      })
-  }, [])
-
-  if(!criptos) return <span>Cargando...</span>
-
   return (
-    <>
-      <h1>Lista de Criptomonedas</h1>
-      <ol>
-        {
-          criptos.map(({id, name, priceUsd}) => (
-            <li key={id}>Nombre: {name} - Precio: {priceUsd}</li>
-          ))
-        }
-      </ol>
-    </>
+    <Cripto/>
   )
 }
 
