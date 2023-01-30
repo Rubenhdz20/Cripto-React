6928161d26b77aed45c3efc5c9bd29bce9d6adf2
Ruben Hernandez Alvarado <rubhernandez42@gmail.com>
2023-01-30 02:55:59 -0600

    cripto table with styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,42 @@
-import Cripto from './Components/Cripto'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import './App.css'
+import Cripto from './Cripto'
 
-function App() {
+const App = () => {
+  const API_URL = import.meta.env.VITE_API_URL
+
+  const [criptos, setCriptos] = useState()
+
+  useEffect(() => {
+    axios.get(`${API_URL}assets`)
+      .then((data) => {
+        setCriptos(data.data.data)
+      })
+      .catch(() => {
+        console.error('La peticion fallo')
+      })
+  }, [])
+
+  if(!criptos) return <span>Cargando...</span>
+
   return (
-    <Cripto/>
+    <div className='app-container'>
+        <h1>Lista de Criptomonedas</h1>
+        <div className='cripto-container'>
+          {
+            criptos.map(({id, name, priceUsd, symbol, changePercent24Hr}) => (
+              <Cripto 
+                key={id} 
+                name={name} 
+                priceUSD={priceUsd} 
+                symbol={symbol} 
+                changePercent24Hr={changePercent24Hr}
+              />
+            ))
+          }
+        </div>
+    </div>
   )
 }
 
